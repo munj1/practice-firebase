d9b96a084136df99a3933b723fecaca0e6d1380f
munj1 <yimunj931@gmail.com>
2022-07-31 18:15:20 +0900

    d

--- pages/firestore/index.js
+++ pages/firestore/index.js
@@ -1,3 +1,4 @@
+import styles from "../../styles/Home.module.css";
 import { useState } from "react";
 import { app, db } from "../../firebase";
 import { useEffect } from "react";
@@ -15,6 +16,8 @@ import {
 
 const Firestore = () => {
   const [data, setData] = useState({});
+  const [show, setShow] = useState(false);
+  const [display, setdisplay] = useState();
   const usersCollectionRef = collection(db, "users");
 
   const handleInput = (event) => {
@@ -35,24 +38,19 @@ const Firestore = () => {
 
   const loadData = async () => {
     // const querySnapshot = await getDocs(usersCollectionRef);
-    // console.log(querySnapshot);
+    // // console.log(querySnapshot);
+    // const queryResult = [];
     // querySnapshot.forEach((doc) => {
-    //   console.log(doc.id);
-    //   console.log(doc.data());
+    //   // console.log(doc.id);
+    //   // console.log(doc.data());
+    //   queryResult.push({ ...doc.data(), id: doc.id });
     // });
 
-    // 일회성이아님 실행할때마다 쭉간다
-    onSnapshot(usersCollectionRef, (data) => {
-      console.log(
-        data.docs.map((item) => {
-          return item.data();
-        })
-      );
-    });
+    // setdisplay(queryResult);
+    setShow(!show);
   };
 
-  const updateData = () => {
-    const docId = "4Xtdj5g8mDlICdNQ2Lv8";
+  const updateData = (docId) => {
     const docToUpdate = doc(db, "users", docId);
     updateDoc(docToUpdate, {
       email: data.email,
@@ -62,9 +60,7 @@ const Firestore = () => {
       .catch((e) => alert(e.message));
   };
 
-  const deleteData = async () => {
-    const querySnapshot = await getDocs(usersCollectionRef);
-    const docId = querySnapshot.docs[0].id;
+  const deleteData = async (docId) => {
     const docToDelete = doc(db, "users", docId);
     deleteDoc(docToDelete)
       .then(() => {
@@ -73,11 +69,30 @@ const Firestore = () => {
       .catch((e) => alert(e.message));
   };
 
+  useEffect(() => {
+    // 일회성이아님 실행할때마다 쭉간다
+    onSnapshot(usersCollectionRef, (data) => {
+      console.log(
+        data.docs.map((item) => {
+          return item.data();
+        })
+      );
+      console.log("display", display);
+      const dataArr = data.docs.map((item) => {
+        return { id: item.id, ...item.data() };
+      });
+      setdisplay(dataArr);
+    });
+  }, []);
+
   return (
-    <div>
+    <div className={styles.container}>
       <Link href="/">
         <button>Back to dashboard</button>
       </Link>
+
+      <h1>[users] firestore db </h1>
+      <h1>Need to be logged in</h1>
 
       <div>
         <input
@@ -92,7 +107,6 @@ const Firestore = () => {
           onChange={(e) => handleInput(e)}
         />
         <button onClick={handleSubmit}>Upload data</button>
-        <button onClick={updateData}>Update data</button>
       </div>
       <div>
         <button onClick={loadData}>Load Data</button>
@@ -100,6 +114,47 @@ const Firestore = () => {
       <div>
         <button onClick={deleteData}>Delete data</button>
       </div>
+
+      {show && display && (
+        <>
+          <div style={{ padding: "10px", marginTop: "10px" }}>
+            Edit:
+            <input
+              name="email"
+              placeholder="Email"
+              onChange={(e) => handleInput(e)}
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => handleInput(e)}
+            />
+          </div>
+
+          <ul>
+            {display.map((user, i) => {
+              return (
+                <li key={user.id} style={{ padding: "10px" }}>
+                  {`${user.email} - ${user.password}`}
+                  <button
+                    style={{ marginLeft: "10px" }}
+                    onClick={() => updateData(user.id)}
+                  >
+                    edit
+                  </button>
+                  <button
+                    style={{ marginLeft: "10px" }}
+                    onClick={() => deleteData(user.id)}
+                  >
+                    delete
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
